47e00c6fa7e27eeea933b592bbd1a92442950d01
Syed Alif <syedmuhdalif18@gmail.com>
2024-06-10 05:57:01 +0800

    s

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -171,8 +171,8 @@
         <div class="hs-dropdown relative inline-flex [--placement:bottom-right]">
           <button id="hs-dropdown-with-header" type="button"
             class="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-            <img class="inline-block size-[38px] rounded-full ring-2 ring-white dark:ring-gray-800" src="/img/dp.jpg"
-              alt="Image Description">
+            <img class="inline-block size-[38px] rounded-full ring-2 ring-white dark:ring-gray-800"
+              src="{{-- /img/dp.jpg --}}" alt="Image Description">
           </button>
 
           <div
